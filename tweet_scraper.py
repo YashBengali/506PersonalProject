eee2ebe40f8e39d2e32ffa2e6f4f57af54a7d745
PatKuz <>
2021-11-08 17:17:40 -0500

    Updated tweets_scraper file to remove duplicate lines in csv, also scraped for more AAPL tweets from last week; total tweets ~60k

--- tweet_scraper.py
+++ tweet_scraper.py
@@ -1,7 +1,9 @@
-import tweepy as tp
 import csv, json
 
 def scrape(key, secret):
+    # Using twitter API limits us to tweets in past week
+    import tweepy as tp
+
     auth = tp.AppAuthHandler(key,secret)
     api = tp.API(auth, wait_on_rate_limit=True)
 
@@ -12,8 +14,28 @@ def scrape(key, secret):
     csvFile.close()
     print('[*] All Done')
 
+def removeDups():
+    csvFile = open("tweets.csv", "r")
+    lines = csvFile.read().split("\n")
+    csvFile.close()
+    
+    noDupsCSV = open("tweets.csv", "w")
+    for line in set(lines):
+        noDupsCSV.write(line + "\n")
+    noDupsCSV.close()
+
+# def scrape():
+#     csvFile = open('tweets.csv', 'w')
+#     csvWriter = csv.writer(csvFile)
+#     csvWriter.writerow(['USERNAME','LINK TO TWEET', 'IS RETWEET', 'TIME', 'TEXT', 'LIKES', 'RETWEETS'])
+#     from twitter_scraper import get_tweets
+#     for tweet in get_tweets('AAPL', pages=30):
+#         csvWriter.writerow([tweet['username'],tweet['tweetUrl'],tweet['isRetweet'],tweet['time'],tweet['text'],tweet['likes'],tweet['retweets']])
+#     csvFile.close()
+#     print('[*] All Done')
+
 if __name__ == "__main__":
-    print('[...] Loading Credentials')
-    creds = json.load(open('credentials.json'))
-    print('[...] Scraping tweets')
-    scrape(creds['key'], creds['secret'])
+    # print('[...] Loading Credentials')
+    # creds = json.load(open('credentials.json'))
+    # scrape(creds['key'], creds['secret'])
+    removeDups()
