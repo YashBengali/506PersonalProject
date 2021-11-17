788cfb207dc1857065eab85347474343fe7efdcf
Conor Walsh <cewalsh@bu.edu>
2021-11-17 01:40:56 -0500

    used google sentiment api and found some correlation, still some issues

--- tweet_scraper.py
+++ tweet_scraper.py
@@ -7,35 +7,35 @@ def scrape(key, secret):
     auth = tp.AppAuthHandler(key,secret)
     api = tp.API(auth, wait_on_rate_limit=True)
 
-    csvFile = open('tweets.csv', 'a')
+    csvFile = open('eth_tweets.csv', 'a')
     csvWriter = csv.writer(csvFile)
-    for tweet in tp.Cursor(api.search_tweets, q='$AAPL').items(500000):
+    for tweet in tp.Cursor(api.search_tweets, q='ethereum').items(500000):
         csvWriter.writerow([tweet.created_at,tweet.user.followers_count,tweet.favorite_count,tweet.text.encode('utf-8')])
     csvFile.close()
     print('[*] All Done')
 
 def removeDups():
-    csvFile = open("tweets.csv", "r")
+    csvFile = open("eth_tweets.csv", "r")
     lines = csvFile.read().split("\n")
     csvFile.close()
     
-    noDupsCSV = open("tweets.csv", "w")
+    noDupsCSV = open("eth_tweets_unique.csv", "w")
     for line in set(lines):
         noDupsCSV.write(line + "\n")
     noDupsCSV.close()
 
 # def scrape():
-#     csvFile = open('tweets.csv', 'w')
+#     csvFile = open('eth_tweets.csv', 'w')
 #     csvWriter = csv.writer(csvFile)
 #     csvWriter.writerow(['USERNAME','LINK TO TWEET', 'IS RETWEET', 'TIME', 'TEXT', 'LIKES', 'RETWEETS'])
 #     from twitter_scraper import get_tweets
-#     for tweet in get_tweets('AAPL', pages=30):
+#     for tweet in get_tweets('ethereum', pages=30):
 #         csvWriter.writerow([tweet['username'],tweet['tweetUrl'],tweet['isRetweet'],tweet['time'],tweet['text'],tweet['likes'],tweet['retweets']])
 #     csvFile.close()
 #     print('[*] All Done')
 
 if __name__ == "__main__":
-    # print('[...] Loading Credentials')
-    # creds = json.load(open('credentials.json'))
-    # scrape(creds['key'], creds['secret'])
+    print('[...] Loading Credentials')
+    creds = json.load(open('credentials.json'))
+    # scrape(creds['api_key'], creds['api_secret_key'])
     removeDups()
